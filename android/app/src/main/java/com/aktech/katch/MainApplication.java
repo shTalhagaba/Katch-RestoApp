06e9aef47286ab5f9e1643f0cc8a26358ecbfa32
Talha <54505302+shTalhagaba@users.noreply.github.com>
2022-09-19 15:41:29 +0500

    Fixed Everything

--- android/app/src/main/java/com/aktech/katch/MainApplication.java
+++ android/app/src/main/java/com/aktech/katch/MainApplication.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import com.aktech.katch.native_modules.ImageBulkLoaderPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.masteratul.exceptionhandler.ReactNativeExceptionHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
